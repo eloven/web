291ad591f5374f5cae7d7e36e77608915c2d2ce0
eloven <guobin201314@gmail.com>
2018-11-25 15:02:44 +0800

    404

--- src/app/core/pages/error404/error404.component.ts
+++ src/app/core/pages/error404/error404.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-error404',
@@ -7,10 +8,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class Error404Component implements OnInit {
 
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit() {
     }
-
+    goHome() {
+        this.router.navigate(['home']);
+    }
 }

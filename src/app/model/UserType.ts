e2311fe4ceb4dd8dc799926418ddf468d37dcc36
eloven <guobin201314@gmail.com>
2018-11-25 08:35:38 +0800

    type

---
+++ src/app/model/UserType.ts
@@ -0,0 +1,13 @@
+/**
+ *  @description UserType
+ *  @author GuoBin201314@gmail.com
+ *  @date 2018-11-24 19:47
+ */
+export enum UserType {
+    Default = 1,
+    Tourist = 2,
+    Concern = 3,
+    Editor = 4,
+    Consumer = 5,
+    Admin = 9,
+}

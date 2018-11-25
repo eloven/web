aa47ea58e6d636407af61db310be85ff5466f521
eloven <guobin201314@gmail.com>
2018-11-25 08:35:32 +0800

    user

---
+++ src/app/model/User.ts
@@ -0,0 +1,12 @@
+/**
+ *  @description User
+ *  @author GuoBin201314@gmail.com
+ *  @date 2018-11-24 19:46
+ */
+
+export class User {
+    id: number;
+    name: string;
+    address: string;
+    userType: number;
+}

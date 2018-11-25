e53810e07a100e5ec376b116f79580e5f9fcf0d9
eloven <guobin201314@gmail.com>
2018-11-25 08:37:49 +0800

    open

---
+++ src/app/core/components/search-bar/search-bar.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+    selector: 'app-search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+    @Output()
+    openSide = new EventEmitter();
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    openSideMenu() {
+        this.openSide.emit();
+    }
+}

66988894d31eac4750ea111873cc7c1fc63b5d5b
eloven <guobin201314@gmail.com>
2018-11-25 12:06:29 +0800

    login-guard

--- src/app/settings/login.guard.ts
+++ src/app/settings/login.guard.ts
@@ -1,14 +1,17 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+    constructor(private auth: AuthService) {}
     canActivate(
         next: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        return true;
+        console.log(this.auth.isAuthenticated());
+        return this.auth.isAuthenticated();
     }
 }

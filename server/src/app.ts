2f16f9dd438371ee63b0bac002f2e8b330c7c6a2
eloven <guobin201314@gmail.com>
2018-12-13 12:34:47 +0800

    express app server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import session from "express-session";
-import bodyParser from "body-parser";
-import cookie from "cookie-parser";
-import path from "path";
-import mongoose from "mongoose";
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import cookie from 'cookie-parser';
+import path from 'path';
+import mongoose from 'mongoose';
 
 // Create Express server
 const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,13 +23,13 @@ app.use((req, res, next) => {
     res.header('name', 'express');
     next();
 });
-app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
  * Primary app routes.
  */
-app.get("/", (req, res, next) => {
-    res.send('hello world express ok')
+app.get('/', (req, res, next) => {
+    res.send('hello world express ok');
 });
 
 

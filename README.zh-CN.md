9bcbb50fca3ae896f91ff1cd3ee2f1dae47a04f1
eloven <guobin201314@gmail.com>
2019-01-08 14:16:59 +0800

    material pro

--- README.zh-CN.md
+++ README.zh-CN.md
@@ -1,14 +1,13 @@
-English | [简体中文](./README.zh-CN.md)
+简体中文 | [English](./README.md)
 
 <h1 align="center">Material Pro</h1>
 
 <div align="center">
 
-simple and practical of Material UI
+开箱即用的Material前端解决方案
 
 [![Build status](https://ci.appveyor.com/api/projects/status/67fxu2by3ibvqtat/branch/master?svg=true)](https://ci.appveyor.com/project/afc163/ant-design-pro/branch/master)
 [![Dependencies](https://img.shields.io/david/ant-design/ant-design-pro.svg)](https://david-dm.org/ant-design/ant-design-pro)
 [![DevDependencies](https://img.shields.io/david/dev/ant-design/ant-design-pro.svg)](https://david-dm.org/ant-design/ant-design-pro?type=dev)
 
 </div>
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,20 @@
-import 'reflect-metadata';
-import '../polyfills';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {HttpClientModule, HttpClient} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-
-// NG Translate
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-
-import {ElectronService} from './providers/electron.service';
-
-import {WebviewDirective} from './directives/webview.directive';
-
-import {AppComponent} from './app.component';
-import {HomeComponent} from './components/home/home.component';
-import {SharedModule} from './shared/shared.module';
-import { CoreModule } from './core/core.module';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        WebviewDirective
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: (HttpLoaderFactory),
-                deps: [HttpClient]
-            }
-        }),
-        CoreModule,
-        SharedModule,
-    ],
-    providers: [ElectronService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }

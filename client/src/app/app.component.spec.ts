118814162c15599fbb871c4ef7c00c964980d72e
GuoBin <844922881@qq.com>
2019-06-26 22:02:00 +0800

    修复【client】（样式）

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -11,7 +11,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then();
   }));
 
   it('should create the app', () => {

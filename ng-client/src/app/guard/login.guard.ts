063087f4aa5aefccf24aced1fc4e1561ff18657d
GuoBin <guobin201314@gmail.com>
2019-11-12 18:20:49 +0800

    layout/guard

--- ng-client/src/app/guard/login.guard.ts
+++ ng-client/src/app/guard/login.guard.ts
@@ -1,14 +1,12 @@
-import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { CanLoad, Route, UrlSegment } from '@angular/router'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true
   }
 }

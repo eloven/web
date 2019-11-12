8741e94fa932ed50f295d4eee630833e32a63db5
GuoBin <guobin201314@gmail.com>
2019-11-12 18:20:19 +0800

    core

--- ng-client/src/app/app-routing.module.ts
+++ ng-client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { LoginComponent } from './layout/login/login.component'
+import { LoginGuard } from './guard/login.guard'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'reactive', pathMatch: 'full' },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive').then(m => m.ReactiveModule),
+    canLoad: [LoginGuard]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ng-client/src/app/core/core.module.ts
+++ ng-client/src/app/core/core.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderComponent, FooterComponent]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule ) {
+    if (parent) {
+      throw new Error('模块已经存在');
+    }
+  }
+}
